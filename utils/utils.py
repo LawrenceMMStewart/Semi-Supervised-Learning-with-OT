81ddc885c2121ae775289cdb862a6b9462d54290
Lawrence Stewart <lawrencestewart@dhcp-18-21-214-223.dyn.mit.edu>
2020-02-22 12:54:24 -0500

    square distance

--- utils/utils.py
+++ utils/utils.py
@@ -10,7 +10,17 @@ import matplotlib.pyplot as plt
 
 def euclidean_dist(X,Y):
     """
-    Calculates the euclidean distance from a 
+    Returns table of pointwise Eucildean Distances
+    C_ij=|| x_i - y_j ||
+
+    Parameters
+    ----------
+    X : (tensor) n x d 
+    Y : (tensor) m x d
+
+    Output
+    ---------
+    C : (tensor) n x m 
 
     """
 
@@ -31,4 +41,42 @@ def euclidean_dist(X,Y):
     Y2=tf.tile(Y2,[n,1])
 
     return tf.cast(tf.math.sqrt(X2+Y2-2.*X@tf.transpose(Y)),tf.float32)
+
+
+
+
+
+def euclidean_sqdist(X,Y):
+    """
+    Returns table of pointwise Eucildean Distances
+    C_ij=|| x_i - y_j ||^2
+
+    Parameters
+    ----------
+    X : (tensor) n x d 
+    Y : (tensor) m x d
+
+    Output
+    ---------
+    C : (tensor) n x m 
+
+    """
+
+    #obtain number of samples for both X and Y 
+    n=X.shape[0]
+    m=Y.shape[0]
+
+    #sum over second dimension for each
+    X2=tf.reduce_sum(X**2,1) # (n,) 
+    Y2=tf.reduce_sum(Y**2,1) # (m,) 
+
+    #add axis 
+    X2=tf.expand_dims(X2,1)  #(n ,1)
+    Y2=tf.expand_dims(Y2,0) # (1,m)
+
+    #broadcast:
+    X2=tf.tile(X2,[1,m])
+    Y2=tf.tile(Y2,[n,1])
+
+    return tf.cast(X2+Y2-2.*X@tf.transpose(Y),tf.float32)
 
