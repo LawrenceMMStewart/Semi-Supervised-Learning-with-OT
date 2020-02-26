fixed masking

toy_examples.py
@@ -7,9 +7,9 @@ from utils.sinkhorn import *
 import tensorflow_probability as tfp
 from tqdm import tqdm
 
-np.random.seed(123)
+np.random.seed(123)	
 
-n_samples=1500
+n_samples=500 #1500
 
 #generate the data
 noisy_circles, _ = datasets.make_circles(n_samples=n_samples,factor=0.5,noise=.05)
@@ -24,45 +24,56 @@ noisy_scurve,_ = datasets.make_s_curve(n_samples=n_samples)
 # plt.show()
 
 #create the observable dataset and mask:
-data,mask=MCAR_Mask(noisy_moons,0.1)
-print(data,mask)
+data,mask=MCAR_Mask(noisy_moons,0.15)
 
 #batchsize
 m=100
 #shuffle data set and partition into subsets of size 2m 
 data,mask=shuffle(data,mask)
-k=int(n_samples/m)
 
-print(data,mask)
+print("percentage of datapoints with missing values ",percentage_missing(mask),"%")
+print("percentage of empty points ",percentage_empty(mask),"%")
+mids,cids=generate_labels(mask)
 
-# #replace nans by means + noise 
-# X=Initialise_Nans(data)
-# #split the data into partitions
-# partitions=np.split(X,k)
 
-# #optimiser
+k=int(data.shape[0]/m)
+
+#replace nans by means + noise 
+X=Initialise_Nans(data)
+#split the data into partitions
+
+partitions=np.array_split(X,k)
+
+#optimiser
 # optimizer=tf.keras.optimizers.RMSprop()
+optimizer=tf.keras.optimizers.Adam()
 
-# imputed_list=[]
+imputed_list=[]
 
-# #for each batch
-# for i in tqdm(range(len(partitions)),desc="Imputation of folds"):
-# 	#seperate into Xk and Xl 
-# 	Xlk=tf.Variable(partitions[i])
-# 	loss_fun = lambda: sinkhorn_sq_batch(Xlk,niter=50)
-# 	losses=tfp.math.minimize(loss_fun,optimizer=optimizer,num_steps=3000)
-# 	imputed_list.append(Xlk.numpy())
+#for each batch
+for i in tqdm(range(len(partitions)),desc="Imputation of folds"):
+	#seperate into Xk and Xl 
+	Xlk=tf.Variable(partitions[i])
+	loss_fun = lambda: sinkhorn_sq_batch(Xlk,niter=50)
 
-# predicted_data=np.concatenate(imputed_list)
-# imputed_data = data*mask+predicted_data*(1-mask)
+	losses=tfp.math.minimize(loss_fun,optimizer=optimizer,num_steps=1000)
+	imputed_list.append(Xlk.numpy())
 
 
-# plt.figure()
-# plt.subplots(121)
-# plt.scatter(noisy_circles[:,0],noisy_circles[:,1])
-# plt.title("Ground truth")
-# plt.subplots(122)
-# plt.scatter(data[:,0],data[:,1],alpha=0.2)
-# plt.scatter(imputed_data[:,0],imputed_data[:,1])
-# plt.title("imputation")
-# plt.show()
+
+predicted_data=np.concatenate(imputed_list)
+imputed_data = np.nan_to_num(data*mask)+predicted_data*(1-mask)
+
+
+
+fig, axes = plt.subplots(1,2)
+axes[0].scatter(noisy_moons[:,0],noisy_moons[:,1])
+axes[0].set_title("ground truth")
+axes[1].scatter(data[cids,0],data[cids,1],alpha=0.8,label="observable")
+axes[1].scatter(imputed_data[mids,0],imputed_data[mids,1],alpha=0.5,label="imputed",marker='x')
+axes[1].legend()
+axes[1].set_title("imputation")
+plt.show()
+
+
+# #to fix -- create a numpy.arraysplit for tensorflow for the sinkhorn algorithm

utils/sinkhorn.py
@@ -92,8 +92,17 @@ def sinkhorn_divergance(x,y,Cost,epsilon=0.01,niter=10):
 
 
 
+# @tf.function
+# def split_tensor(X,K):
+#     return tf.numpy_function(np.array_split,[X,K],Tout=tf.float32)
+
+
+
+
 def sinkhorn_sq_batch(X,epsilon=0.01,niter=10):
-    X1,X2=tf.split(X,2)
+    #split batch in half
+    X1,X2 = tf.split(X,2)
+    #caluclate sinkhorn divergance
     return sinkhorn_divergance(X1,X2,euclidean_sqdist,epsilon=epsilon,niter=niter)
 
 

utils/utils.py
@@ -81,9 +81,27 @@ def euclidean_sqdist(X,Y):
     return tf.cast(X2+Y2-2.*X@tf.transpose(Y),tf.float32)
 
 
+# @tf.function(input_signature=[tf.TensorSpec(tf.float32,tf.int32)]) 
+# def tensor_split(X,K):
+#     """
+#     Splits a tensor X into k tensors along axis=0 as best as possible
+
+#     Parameters
+#     -----------
+#     X : float32 tensor (n x d) 
+#     k : tensor
 
 
-def MCAR_Mask(data,p):
+#     Output
+#     ----------
+#     L : List of tensors
+
+#     """
+#     return tf.numpy_function(np.array_split,[X,K],Tout=tf.float32)
+
+
+#please fix bug here
+def MCAR_Mask(data,p,del_empty=False):
     """
     Randomly masks values of data with probability p
     i.e. Missing Completely At Random
@@ -110,11 +128,15 @@ def MCAR_Mask(data,p):
     nan_mask[nan_mask==0]=np.nan
     obs_data=data*nan_mask
 
-    #deal with the case where a datapoint is empty:
-    for i in range(n):
-        if (obs_data[i]==np.nan).all():
-            obs_data=np.delete(obs_data,(i),axis=0)
-            mask=np.delete(mask,(i),axis=0)
+    if del_empty:
+        #deal with the case where a datapoint is empty:
+        to_delete=[]
+        for i in range(n):
+            if np.isnan(obs_data[i]).all():
+                to_delete.append(i)
+        #remove empty datapoints
+        obs_data=np.delete(obs_data,to_delete,axis=0)
+        mask=np.delete(mask,to_delete,axis=0)
 
     return obs_data,mask
 
@@ -146,6 +168,68 @@ def Initialise_Nans(data,η=0.1):
 
     return filled
 
+
+
+
+def percentage_missing(mask):
+    """
+    Returns the percentage of the data that has missing values
+
+    Parameters
+    ----------
+    mask : np.array (n x d) {0,1}
+    
+    Output
+    ----------
+    percentage: float
+
+    """
+
+    labels=[1 if 0 in mask[i] else 0 for i in range(len(mask))]
+    return np.mean(labels)
+
+def percentage_empty(mask):
+    """
+    Returns the percentage of the data that is empty
+    i.e. each point of the data is missing
+
+    Parameters
+    ----------
+    mask : np.array (n x d) {0,1}
+    
+    Output
+    ----------
+    percentage: float
+
+    """
+    labels=[1 if (mask[i]==0).all() else 0 for i in range(len(mask))]
+    return np.mean(labels)
+
+def generate_labels(mask):
+    """
+    Returns two lists, the first consisting of the indicies
+    of the data points who are missing values, and the second
+    the indices of data points without missing values
+
+    Parameters
+    ----------
+    mask : np.array (n x d) {0,1}
+
+    Output
+    ----------
+    missing_ids : int list
+    complete_ids : int list
+
+    """
+    missing_ids=[]
+    complete_ids=[]
+    for i in range(len(mask)):
+        if 0 in mask[i]:
+            missing_ids.append(i)
+        else:
+            complete_ids.append(i)
+
+    return missing_ids,complete_ids
 
 
 