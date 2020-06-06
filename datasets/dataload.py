72d744716c6fce20c20b7e3873cf13374e717495
Lawrence Stewart <lawrencestewarthome@gmail.com>
2020-06-06 20:19:18 +0100

    skill craft dataset added

--- datasets/dataload.py
+++ datasets/dataload.py
@@ -17,6 +17,32 @@ import os
 np.random.seed(123)
 
 
+
+def create_name(varlist,taglist):
+    """
+    creates a file name for a set of parameterw
+    e.g.  varlist = [10,2]  tag_list = ['n','m']
+    	  output = 'n10-m2'
+
+
+    Parameters
+    ----------
+    varlist : list size n 
+    taglist : str list size n
+
+    Outputs
+    ---------
+    run_tag : str
+    """
+    
+    assert len(varlist)==len(taglist)
+    run_tag = taglist[0]+str(varlist[0])
+    for i in range(1,len(varlist)):
+        run_tag+="-"+taglist[i]+str(varlist[i])
+    return run_tag
+
+
+
 def load_wine(train_size=4000):
 	"""
 	Loads the white wine dataset
@@ -153,5 +179,78 @@ def load_housing(train_size = 450):
 	return train,test,train_y,test_y
 
 
+
+def load_skillcraft(train_size=3000):
+	"""
+	Loads the skillcraft dataset
+
+	Attributes
+	1. GameID: Unique ID number for each game (integer)
+	2. LeagueIndex: Bronze, Silver, Gold, Platinum, ... coded 1-8 (Ordinal)
+	3. Age: Age of each player (integer)
+	4. HoursPerWeek: Reported hours spent playing per week (integer)
+	5. TotalHours: Reported total hours spent playing (integer)
+	6. APM: Action per minute (continuous)
+	7. SelectByHotkeys: No. of unit or building selections made using hotkeys per timestamp (cts)
+	8. AssignToHotkeys: No. of units or buildings assigned to hotkeys per timestamp (cts)
+	9. UniqueHotkeys: No. of unique hotkeys used per timestamp (cts)
+	10. MinimapAttacks: No. of attack actions on minimap per timestamp (cts)
+	11. MinimapRightClicks: number of right-clicks on minimap per timestamp (cts)
+	12. NumberOfPACs: No. of PACs per timestamp (cts)
+	13. GapBetweenPACs: Mean duration in milliseconds between PACs (cts)
+	14. ActionLatency: Mean latency from the onset of a PACs to their first action in milliseconds (cts)
+	15. ActionsInPAC: Mean number of actions within each PAC (cts)
+	16. TotalMapExplored: The number of 24x24 game coordinate grids viewed by the player per timestamp (cts)
+	17. WorkersMade: No. of SCVs, drones, and probes trained per timestamp (cts)
+	18. UniqueUnitsMade: Unique unites made per timestamp (cts)
+	19. ComplexUnitsMade: No. of ghosts, infestors, and high templars trained per timestamp (cts)
+	20. ComplexAbilitiesUsed: Abilities requiring specific targeting instructions used per timestamp (cts)
+
+	Parameters
+	----------
+	train_size : int (train test split)
+
+	Output
+	--------
+	train : array
+	test : array
+	train_y : array
+	test_y : array
+	"""
+
+	#initialisations for dataset
+	scaler = MinMaxScaler()  
+	path = os.path.join("datasets","skillcraft","SkillCraft1_Dataset.csv")
+	data = pd.read_csv(path,sep=',')
+
+	drops = ["GameID"]
+	scraft_df = data.drop(drops,axis=1)
+
+	#ids of data with no missing values
+	full_data_ids  = (scraft_df!='?').all(axis=1)
+	scraft_df  =scraft_df[full_data_ids]
+
+	X = scraft_df.drop("LeagueIndex",axis=1)
+	Y = scraft_df.LeagueIndex
+
+	#fit the scaler to X for preprocessing
+	scaler.fit(X)
+
+	#split into train and test sets
+	train_x,test_x,train_y,test_y = train_test_split(X,Y,
+		random_state = 0, stratify = Y,shuffle=True,
+		train_size=train_size)
+
+	#scale the data using sklearn Minmax scalar
+	train  = scaler.transform(train_x)
+	test   = scaler.transform(test_x)
+	
+	#reshape y's from (n,) --> (n,1)
+	train_y  = pd.DataFrame.to_numpy(train_y).reshape(-1,1).astype(np.float32)
+	test_y  = pd.DataFrame.to_numpy(test_y).reshape(-1,1).astype(np.float32)
+
+	return train,test,train_y,test_y
+
+	
 
 
